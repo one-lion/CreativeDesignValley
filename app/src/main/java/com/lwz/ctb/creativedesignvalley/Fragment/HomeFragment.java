99acace2352380588f1831bbe5aa331b96dba852
ctb <1763190242@qq.com>
2019-11-03 01:09:40 +0800

    描述:1.新增了首页搜索框; 2.新增了平面设计栏

--- app/src/main/java/com/lwz/ctb/creativedesignvalley/Fragment/HomeFragment.java
+++ app/src/main/java/com/lwz/ctb/creativedesignvalley/Fragment/HomeFragment.java
@@ -3,12 +3,15 @@ package com.lwz.ctb.creativedesignvalley.Fragment;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.lwz.ctb.creativedesignvalley.HomePagerActivity.HomePagerSearchActivity;
 import com.lwz.ctb.creativedesignvalley.R;
 
 /**
@@ -18,9 +21,11 @@ import com.lwz.ctb.creativedesignvalley.R;
 
 //这个是首页的碎片
 @SuppressLint("ValidFragment")
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment  implements View.OnClickListener{
 
     private Context context;      //活动实例
+    private View homeView;     //首页视图
+    private ImageView ivHomeSearch; //搜素框
 
     @SuppressLint("ValidFragment")
     public HomeFragment(Context context) {
@@ -33,9 +38,34 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_home, container, false);
+        homeView=view;
 
-
+        init();
+        setListener();
         return view;
     }
+
+    private void init(){
+        ivHomeSearch=homeView.findViewById(R.id.iv_home_search);
+    }
+
+    private void setListener(){
+        ivHomeSearch.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.iv_home_search:
+                Intent intent=new Intent(context, HomePagerSearchActivity.class);
+                startActivity(intent);
+                break;
+
+                default:
+                    break;
+        }
+    }
+
 
 }
---
+++ app/src/main/java/com/lwz/ctb/creativedesignvalley/HomePagerActivity/HomePagerSearchActivity.java
@@ -0,0 +1,56 @@
+package com.lwz.ctb.creativedesignvalley.HomePagerActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.lwz.ctb.creativedesignvalley.R;
+
+
+//import scut.carson_ho.searchview.ICallBack;
+import com.lwz.ctb.creativedesignvalley.Widget.SearchView;
+
+import scut.carson_ho.searchview.ICallBack;
+import scut.carson_ho.searchview.bCallBack;
+//import scut.carson_ho.searchview.bCallBack;
+
+public class HomePagerSearchActivity extends AppCompatActivity {
+
+    //初始化搜索框变量
+    private SearchView homePagerSearchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_pager_search);
+
+        init();
+        setListener();
+
+    }
+
+    private void init(){
+        homePagerSearchView=findViewById(R.id.sv_homepager_search); //初始化首页搜索框
+    }
+
+    private void setListener(){
+        //设置点击搜索按键后的操作
+        //通过回调函数
+        homePagerSearchView.setOnClickSearch(new ICallBack() {
+            @Override
+            public void SearchAciton(String string) {      //这里的文本就是搜索的内容
+                Log.d("首页搜索文本", string);
+            }
+        });
+
+        //设置点击返回键后的操作，通过回调函数实现
+        homePagerSearchView.setOnClickBack(new bCallBack() {
+            @Override
+            public void BackAciton() {
+                finish();
+            }
+        });
+    }
+
+
+}
